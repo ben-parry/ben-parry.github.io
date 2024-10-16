870bb703896dc421ccdc4b8a4468170b8ae4ccac
ben-parry <ben.parry@hey.com>
2024-10-15 20:55:39 -0700

    Major Updates to content and a little formatting

--- dark.js
+++
@@ -1,6 +0,0 @@
-const darkModeImage = document.querySelector('#darkModeToggle');
-const htmlBody = document.querySelector('body');
-
-darkModeImage.addEventListener('click', () => {
-  htmlBody.classList.toggle('dark-mode');
-});
