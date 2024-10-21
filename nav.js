d74250e17acc47a85b06508f89d5d3b7cde2d4d3
ben-parry <ben.parry@hey.com>
2024-10-21 11:06:52 -0400

    Fixes

--- nav.js
+++ nav.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 <li><a href="https://www.benparry.ca/complexity-notebooks">Complexity Notebooks</a></li>
                 <li><a href="https://philosophyofprogress.com/">Progress Seminar</a></li>
                 <li><a href="https://skillfulnotes.com/">Skillful Notes</a></li>
+                <li><a href="https://benparry.substack.com//">Blog</a></li>
                 <li><a href="https://twitter.com/Via_Benjamin">X (Twitter)</a></li>
             </ul>
         </nav>
