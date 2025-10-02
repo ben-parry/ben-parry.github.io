a37439c610fe6aaeb6da2adfd552e871e52edd46
ben-parry <ben.parry@hey.com>
2025-10-02 15:18:24 -0400

    Resume

--- nav.js
+++ nav.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 <li><a href="https://www.benparry.ca/complexity-notebooks">Complexity Notebooks</a></li>
                 <li><a href="https://www.benparry.ca/essays">Essayists</a></li>
                 <li><a href="https://www.philosophyofprogress.com/">Progress Seminar</a></li>
+                <li><a href="https://www.benparry.ca/ben-parry-resume.pdf">Resume</a></li>
                 <li><a href="https://skillfulnotes.com/">Skillful Notes</a></li>
                 <li><a href="https://twitter.com/_benjaminparry">X (Twitter)</a></li>
             </ul>
